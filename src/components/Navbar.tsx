4cc8f5dba3164027d96543741d01efcbb00fc248
Ariel Galvez <arielgp.70@gmail.com>
2023-07-27 21:41:35 -0400

    move navbar to layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,56 +31,54 @@ const Navbar = () => {
 		});
 	};
 
-	const memorizedItems = useMemo(
-		() => (
+	const memorizedItems = useMemo(() => {
+		const baseUrl = `${globalThis.window?.location?.origin}/portfolio/`;
+		return (
 			<>
-				<NavbarItem title='About' href='#about' />
-				<NavbarItem title='Experience' href='#experience' />
-				<NavbarItem title='Projects' href='#projects' />
-				<NavbarItem title='Contact' href='#contact' />
-				{/* <button onClick={() => setTheme()}>{colorTheme}</button> */}
+				<NavbarItem title='About' href={`${baseUrl}#about`} />
+				<NavbarItem title='Experience' href={`${baseUrl}#experience`} />
+				<NavbarItem title='Projects' href={`${baseUrl}#projects`} />
+				<NavbarItem title='Contact' href={`${baseUrl}#contact`} />
 				<DarkModeSwitch theme={colorTheme} onClick={setTheme} />
 			</>
-		),
-		[colorTheme, setTheme]
-	);
+		);
+	}, [colorTheme, setTheme, globalThis.window]);
 
 	return (
 		<>
-			<header className='sticky top-0 backdrop-blur-sm'>
+			<header className='sticky top-0 backdrop-blur-sm z-10 px-3 md:px-5 max-w-5xl mx-auto'>
 				<nav className='flex justify-between items-center'>
 					<ul className='flex gap-3'>
 						<SocialMedia />
 					</ul>
-					<ul className='hidden gap-6 items-center sm:flex'>{memorizedItems}</ul>
+					<ul className='hidden gap-4 items-center md:flex'>{memorizedItems}</ul>
 				</nav>
 			</header>
-			<div className='fixed top-2 right-2 z-10'>
+			<div className='fixed top-2 right-2 z-20'>
 				<button
 					id='menu'
-					className={`relative w-9 h-8 p-1 cursor-pointer sm:hidden
-					`}
+					className='relative w-9 h-8 p-1 cursor-pointer md:hidden group'
 					onClick={handleClick}
 				>
 					<span
-						className={`absolute block w-4 h-1 bg-black dark:bg-white rounded-sm sibling-checked:bg-blue-500 ${
+						className={`absolute block w-4 h-[3px] bg-black group-hover:bg-blue-500 dark:bg-white rounded-md sibling-checked:bg-blue-500 ${
 							isOpen ? 'w-8 rotate-45 top-5' : 'top-2'
 						} transition-all`}
 					></span>
 					<span
-						className={`absolute block top-4 w-6 h-1 bg-black dark:bg-white rounded-sm ${
+						className={`absolute block top-4 w-6 h-[3px] bg-black group-hover:bg-blue-500 dark:bg-white rounded-md ${
 							isOpen && 'opacity-0'
 						} transition-all`}
 					></span>
 					<span
-						className={`absolute block w-8 h-1 bg-black dark:bg-white rounded-sm ${
+						className={`absolute block w-8 h-[3px] bg-black group-hover:bg-blue-500 dark:bg-white rounded-md ${
 							isOpen ? 'rotate-[-45deg] top-5' : 'top-6'
 						} transition-all`}
 					></span>
 				</button>
 			</div>
 			{isOpen && (
-				<div className='fixed top-0 right-0 w-[100vw] h-[100vh] bg-[rgba(0,0,0,0.1)] sm:hidden overflow-hidden'>
+				<div className='fixed top-0 right-0 w-[100vw] h-[100vh] bg-[rgba(0,0,0,0.1)] md:hidden overflow-hidden z-10'>
 					<ul
 						ref={menuRef}
 						className='absolute right-0 bg-[var(--bg-color)]
